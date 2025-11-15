270e70d7a01c30d5f59f287e7fc9a05d778c60f1
Alex Evanczuk <alexevanczuk@gmail.com>
2025-11-15 18:35:25 -0500

    Fix "go to definition" on it_behaves_like
    [selective-tests]

--- rewriter/Minitest.cc
+++ rewriter/Minitest.cc
@@ -921,7 +921,7 @@ ast::ExpressionPtr runSingle(core::MutableContext ctx, bool isClass, const ast::
             // Include the shared examples module in this isolated context
             auto sharedExamplesName = makeSharedExamplesConstant(ctx, arg);
             auto includeStmt = ast::MK::Send1(send->loc, ast::MK::Self(send->recv.loc()), core::Names::include(),
-                                              send->funLoc, move(sharedExamplesName));
+                                              arg.loc(), move(sharedExamplesName));
 
             ast::ClassDef::RHS_store rhs;
             rhs.emplace_back(move(includeStmt));
